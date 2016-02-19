9f1e065bdd761b51fdba7b9195e3d989ea85e76b
Pedro Costa <pedro@plcosta.com>
2016-02-19 14:22:35 -0300

    Change store to use Vuex

--- src/store.js
+++ src/store.js
@@ -1,5 +1,17 @@
-export default {
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+var store = new Vuex.Store({
   state: {
     counter: 0
+  },
+  mutations: {
+    INCREMENT (state) {
+      state.counter ++
+    }
   }
-}
+})
+
+export default store
